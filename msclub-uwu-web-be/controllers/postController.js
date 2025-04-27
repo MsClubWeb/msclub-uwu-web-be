511948247dc09776f5bf4708f6c2d76c725e0c28
AThusithaGayan <thusithag@aplicy.com>
2025-04-27 23:59:25 +0530

    fix : add reqest data

--- msclub-uwu-web-be/controllers/postController.js
+++ msclub-uwu-web-be/controllers/postController.js
@@ -14,18 +14,25 @@ exports.getAllPosts = async (req, res) => {
 
 // CREATE post (admin only)
 exports.createPost = async (req, res) => {
-  const { title, content, category } = req.body;
+  const {
+    category,
+    title,
+    bannerDescription,
+    bannerImage,
+    date,
+    authorName,
+    description
+  } = req.body;
 
   try {
-    // generate next postId by finding the max current one
-    const lastPost = await Post.findOne({ order: [['postId', 'DESC']] });
-    const nextPostId = lastPost ? lastPost.postId + 1 : 1;
-
     const post = await Post.create({
-      title,
-      content,
       category,
-      postId: nextPostId
+      title,
+      bannerDescription,
+      bannerImage,
+      date,
+      authorName,
+      description
     });
     res.status(201).json(post);
   } catch (err) {
@@ -37,15 +44,28 @@ exports.createPost = async (req, res) => {
 // UPDATE post (admin only)
 exports.updatePost = async (req, res) => {
   const { id } = req.params;
-  const { title, content, category } = req.body;
+  const {
+    category,
+    title,
+    bannerDescription,
+    bannerImage,
+    date,
+    authorName,
+    description
+  } = req.body;
 
   try {
     const post = await Post.findByPk(id);
     if (!post) return res.status(404).json({ message: 'Post not found' });
 
-    post.title = title;
-    post.content = content;
     post.category = category;
+    post.title = title;
+    post.bannerDescription = bannerDescription;
+    post.bannerImage = bannerImage;
+    post.date = date;
+    post.authorName = authorName;
+    post.description = description;
+
     await post.save();
 
     res.json(post);
